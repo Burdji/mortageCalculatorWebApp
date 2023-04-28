ae50624cafe5bb31b3210cb675927d7ca2324c81
burdini <skivai@abv.bg>
2023-04-28 04:13:00 +0300

    Finished the appearance of the app.
    Adjusted the text and slider sizes and positioning and adjusted the button to be more similar to the other elements.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let purchPrice = document.querySelector('#purchPrice');
 let purchPriceValue = document.querySelector('#purchPriceValue');
 let downPay = document.querySelector('#downPay');
@@ -11,22 +10,39 @@ let sliders= document.querySelector('.box');
 let loan = document.getElementById('loan');
 let montlyPay = document.getElementById('montlyPay');
 const update = () => {
-    intRateValue.innerHTML = intRate.value;
+    intRateValue.innerHTML = addProcentSign(intRate.value);
     intRate.style.background = 'linear-gradient(90deg, rgb(255,188,3)'+intRate.value*10+'%, rgb(255,255,255)'+intRate.value*10+'%)';
     repayTimeValue.innerHTML = repayTime.value;
     repayTime.style.background = 'linear-gradient(90deg, rgb(255,188,3)'+repayTime.value*3.33+'%, rgb(255,255,255)'+repayTime.value*3.33+'%)';
-    purchPriceValue.innerHTML = numberWithCommas(purchPrice.value);
+    purchPriceValue.innerHTML = addSpacesAndCommas(purchPrice.value);
     purchPrice.style.background ='linear-gradient(90deg, rgb(255,188,3)'+purchPrice.value/10000+'%, rgb(255,255,255)'+purchPrice.value/10000+'%)';
     downPay.max = purchPrice.value;
-    downPayValue.innerHTML = numberWithCommas(downPay.value);
+    downPayValue.innerHTML = addSpacesAndCommas(downPay.value);
     let pro = purchPrice.value/100;
     downPay.style.background ='linear-gradient(90deg, rgb(255,188,3)'+downPay.value/pro+'%, rgb(255,255,255)'+downPay.value/10000+'%)';
     let a = purchPrice.value-downPay.value;
-    loan.innerHTML = numberWithCommas(a);
-    montlyPay.innerHTML = numberWithCommas(Math.round((a + (a*repayTime.value * intRate.value)/100)/(repayTime.value * 12)));
-}
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    loan.innerHTML = addSpacesAndCommas(a);
+    montlyPay.innerHTML = addSpacesAndCommas(Math.round((a + (a*repayTime.value * intRate.value)/100)/(repayTime.value * 12)));
 }
 sliders.addEventListener('input',update)
 update()
+function addSpacesAndCommas(x){
+    x=x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    while(x.length<9){
+       x=x+' ';
+    }
+    while(x.includes(' ')){
+        x=x.replace(' ','&ensp;')
+    }
+    return x;
+}
+function addProcentSign(x){
+    x=x+'%'
+    while(x.length<5){
+        x=x+' ';
+    }
+    while(x.includes(' ')){
+        x=x.replace(' ','&ensp;');
+    }
+    return x;
+}
